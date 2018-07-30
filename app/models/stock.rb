a2a0cb9929555bb7827860ef12ec66e1d60c6f40
psidae <akson.rill@gmail.com>
2018-07-30 18:43:18 +0300

    search by ticker

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
 class Stock < ApplicationRecord
+  def self.new_from_lookup(ticker_symbol)
+    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+    new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, price: looked_up_stock.latest_price)
+  rescue Exception => e
+    nil
+  end
 end
